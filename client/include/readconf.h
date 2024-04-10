e6dca8b316b5681991d741e0e1e518061f92cd63
xyzwmdzyx <wmd.de.zhanghu@gmail.com>
2024-04-10 20:58:01 +0800

    commit file

--- client/include/readconf.h
+++ client/include/readconf.h
@@ -15,21 +15,85 @@
 #ifndef _READ_CONF_H_
 #define _READ_CONF_H_
 
-#define HOSTNAME_LEN	256
-#define CLIENTID_LEN    128
-#define USERNAME_LEN    128
-#define PASSWORD_LEN    128
+typedef struct log_conf_s {
 
-typedef struct conf_s {
+    char        logfile[64];            // log path     
+    int         loglevel;               // log level
+    int         logsize;                // log max size
+
+}log_conf_t;
+
+typedef struct hardware_conf_s {
+
+    char            deviceid[16];       // device id
+    int             ds18b20;            // ds18b20 = 1 means this hardware exist
+
+}hardware_conf_t;
+
+typedef struct broker_conf_s {
     
-    char        	host[HOSTNAME_LEN];         // broker hostname
-    int         	port;                       // broker port
-    char            clientid[CLIENTID_LEN];     // client id
-    char            username[USERNAME_LEN];     // user name
-    char            password[PASSWORD_LEN];     // pass word
+    char        	host[256];          // broker hostname
+    int         	port;               // broker port
+    char            clientid[128];      // client id
+    char            username[128];      // user name
+    char            password[128];      // pass word
 
-}conf_t;
+}broker_conf_t;
 
-int readConf(char *confile, conf_t *conf);
+typedef struct puber_conf_s {
+
+    char            pubtopic[256];      // publish topic
+    int             readtime;           // sample interval tim
+
+}puber_conf_t;
+
+
+/*	description:	get configurations
+ *	 input args:	
+ *					$confile  : configure file path
+ * return value:    <0: failure   0: success
+ */
+extern int readConf(char *confile);
+
+
+/*****************************************************
+*lower API, will just been used by fuction readConf()*
+******************************************************/
+
+
+/*	description:	get log system configurations
+ *	 input args:	
+ *					$confile  : configure file path
+ *                  $log_conf : log system configurations struct 
+ * return value:    <0: failure   0: success
+ */
+int readLogConf(char *confile, log_conf_t *log_conf);
+
+
+/*	description:	get hardware configurations
+ *	 input args:	
+ *					$confile  : configure file path
+ *                  $log_conf : hardware configurations struct 
+ * return value:    <0: failure   0: success
+ */
+int readHardWareConf(char *confile, hardware_conf_t *hard_conf);
+
+
+/*	description:	get mosquitto mqtt broker configurations
+ *	 input args:	
+ *					$confile  : configure file path
+ *                  $log_conf : mosquitto mqtt broker configurations struct 
+ * return value:    <0: failure   0: success
+ */
+int readBrokerConf(char *confile, broker_conf_t *bro_conf);
+
+
+/*	description:	get mosquitto mqtt publisher configurations
+ *	 input args:	
+ *					$confile  : configure file path
+ *                  $log_conf : mosquitto mqtt publisher configurations struct 
+ * return value:    <0: failure   0: success
+ */
+int readPubConf(char *confile, puber_conf_t *pub_conf);
 
 #endif
--- client/src/readconf.c
+++ client/src/readconf.c
@@ -11,6 +11,225 @@
  *                 
  ********************************************************************************/
 
-
+#include <string.h>
 #include "readconf.h"
+#include "logger.h"
 
+
+/*	description:	get log system configurations
+ *	 input args:	
+ *					$confile  : configure file path
+ *                  $log_conf : log system configurations struct 
+ * return value:    <0: failure   0: success
+ */
+int readLogConf(char *confile, log_conf_t *log_conf) {
+
+    char    line[256];
+    int     flag = 0;
+    char    *key = NULL;
+    char    *value = NULL;
+    
+    // check input args
+    if( !confile || !log_conf ) {
+        logError("fuction %s() gets invalid input args\n", __func__);
+        flag = -1;
+        goto Cleanup;
+    }
+
+    // read configurations by line
+    memset(line, 0, sizeof(line));
+    while(fgets(line, sizeof(line), confile)) {
+
+        // remove '\n'
+        line[strcspn(line, "\n")] = '\0';
+
+        // check if this section is log
+        if( !strcmp(line, "[log]") ) {
+            flag = 1;
+            continue;
+        }
+
+        // read key and value
+        if( flag ) {
+
+            // check if this line is a new section
+            if(line[0] == '[') {
+                flag = 0;
+                break;
+            }
+
+            // parse key and value
+            key = strtok(line, "=");
+            value = strtok(NULL, "=");
+            if( key && value ) {
+                if( !strcmp(key, "logpath") ) {
+                    strncpy(log_conf->logfile, value, sizeof(log_conf->logfile));
+                }
+                else if( !strcmp(key, "loglevel") ) {
+                    log_conf->loglevel = atoi(value);
+                }
+                else if( !strcmp(key, "logsize") ) {
+                    log_conf->logsize = atoi(value);
+                }
+                else {
+                    logError("invalid key whitch is not been allowed\n");
+                    flag = -3;
+                    goto Cleanup;
+                }
+            }
+            else {
+                logError("can't read key or value form this section\n");
+                flag = -2;
+                goto Cleanup;
+            }
+        }
+
+    }
+
+ Cleanup:
+    return flag;
+}
+
+
+/*	description:	get hardware configurations
+ *	 input args:	
+ *					$confile  : configure file path
+ *                  $log_conf : hardware configurations struct 
+ * return value:    <0: failure   0: success
+ */
+int readHardWareConf(char *confile, hardware_conf_t *hard_conf) {
+
+    char    line[256];
+    int     flag = 0;
+    char    *key = NULL;
+    char    *value = NULL;
+    
+    // check input args
+    if( !confile || !hard_conf ) {
+        logError("fuction %s() gets invalid input args\n", __func__);
+        flag = -1;
+        goto Cleanup;
+    }
+
+    // read configurations by line
+    memset(line, 0, sizeof(line));
+    while(fgets(line, sizeof(line), confile)) {
+
+        // remove '\n'
+        line[strcspn(line, "\n")] = '\0';
+
+        // check if this section is hardware
+        if( !strcmp(line, "[hardware]") ) {
+            flag = 1;
+            continue;
+        }
+
+        // read key and value
+        if( flag ) {
+
+            // check if this line is a new section
+            if(line[0] == '[') {
+                flag = 0;
+                break;
+            }
+
+            // parse key and value
+            key = strtok(line, "=");
+            value = strtok(NULL, "=");
+            if( key && value ) {
+                if( !strcmp(key, "deviceid") ) {
+                    strncpy(hard_conf->deviceid, value, sizeof(hard_conf->deviceid));
+                }
+                else if( !strcmp(key, "ds18b20") ) {
+                    hard_conf->ds18b20 = atoi(value);
+                }
+                else {
+                    logError("invalid key whitch is not been allowed\n");
+                    flag = -3;
+                    goto Cleanup;
+                }
+            }
+            else {
+                logError("can't read key or value form this section\n");
+                flag = -2;
+                goto Cleanup;
+            }
+        }
+
+    }
+
+ Cleanup:
+    return flag;
+}
+
+
+/*	description:	get mosquitto mqtt configurations
+ *	 input args:	
+ *					$confile  : configure file path
+ *                  $log_conf : mosquitto mqtt configurations struct 
+ * return value:    <0: failure   0: success
+ */
+int readBrokerConf(char *confile, broker_conf_t *bro_conf) {
+
+    char    line[256];
+    int     flag = 0;
+    char    *key = NULL;
+    char    *value = NULL;
+    
+    // check input args
+    if( !confile || !mqtt_conf ) {
+        logError("fuction %s() gets invalid input args\n", __func__);
+        flag = -1;
+        goto Cleanup;
+    }
+
+    // read configurations by line
+    memset(line, 0, sizeof(line));
+    while(fgets(line, sizeof(line), confile)) {
+
+        // remove '\n'
+        line[strcspn(line, "\n")] = '\0';
+
+        // check if this section is hardware
+        if( !strcmp(line, "[broker]") ) {
+            flag = 1;
+            continue;
+        }
+
+        // read key and value
+        if( flag ) {
+
+            // check if this line is a new section
+            if(line[0] == '[') {
+                flag = 0;
+                break;
+            }
+
+            // parse key and value
+            key = strtok(line, "=");
+            value = strtok(NULL, "=");
+            if( key && value ) {
+                if( !strcmp(key, "deviceid") ) {
+                    strncpy(hard_conf->deviceid, value, sizeof(hard_conf->deviceid));
+                }
+                else if( !strcmp(key, "ds18b20") ) {
+                    hard_conf->ds18b20 = atoi(value);
+                }
+                else {
+                    logError("invalid key whitch is not been allowed\n");
+                    flag = -3;
+                    goto Cleanup;
+                }
+            }
+            else {
+                logError("can't read key or value form this section\n");
+                flag = -2;
+                goto Cleanup;
+            }
+        }
+
+    }
+
+ Cleanup:
+    return flag;
+}
